112dca67424a3497dc99a0f9ace491c2f8c9959d
MRXPRO200330020179Y <mrxpro2003@gmail.com>
2023-04-17 14:46:17 +0500

    changed

--- dist/js/index.js
+++ dist/js/index.js
@@ -137,6 +137,22 @@ $(function(){
 // -------------------------------===========-------------------------------
 
 $(function(){
+  let Catalog_max__pro__ul_link = document.querySelectorAll('.library__link__menu');
+
+  for(let i = 0; i<Catalog_max__pro__ul_link.length; i++){
+      Catalog_max__pro__ul_link[i].addEventListener('click',function(){
+          for(let j = 0; j<Catalog_max__pro__ul_link.length;j++){
+              Catalog_max__pro__ul_link[j].classList.remove('active');
+          }
+          this.classList.add('active');
+
+      })
+  }
+});
+
+// -------------------------------===========-------------------------------
+
+$(function(){
   let Catalog_max__pro__ul_link = document.querySelectorAll('.newsAll__pagination__link');
 
   for(let i = 0; i<Catalog_max__pro__ul_link.length; i++){
